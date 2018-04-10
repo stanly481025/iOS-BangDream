0966e5fb3a354e0c36931c59a237122d2cb67537
stanlyChen <stanly481025@gmail.com>
2018-04-10 21:12:47 +0800

    iOS HW

---
+++ iOS_HW1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iOS_HW1
+//
+//  Created by user_02 on 2018/4/9.
+//  Copyright © 2018年 yusiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        Thread.sleep(forTimeInterval: 1.0)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
